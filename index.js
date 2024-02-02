c4883d49e2382a6f3e6a2b98656e0bd1ed141cc8
MDz1985 <m_d@mail.uk>
2024-02-02 14:18:54 +0100

    feat: add necessary functions to use in the whole app

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { chdir } from 'node:process';
 import { homedir } from 'node:os'
 import { showUserName } from "./modules/user.js";
+import { checkInput, showCurrentDirectory } from "./services/file-manager-service.js";
 
 const usernameRegex = /--username=.*/
 const username = process.argv.find((el) => usernameRegex.test(el))?.split('=')[1];
 chdir(homedir());
 
 showUserName(username);
+checkInput(username);
+showCurrentDirectory();
---
+++ services/file-manager-service.js
@@ -0,0 +1,44 @@
+import { commands } from '../modules/commands.js';
+import { stdin } from 'node:process';
+
+export function checkInputData(data) {
+  [...commands.entries()].every(([func, regex], i) => {
+    if (regex.test(data)) {
+      const spacePosition = data.indexOf(' ');
+      const argument = spacePosition > 0 ? data.slice(spacePosition + 1).toString() : null;
+      func(argument);
+      return false;
+    }
+    if (i === commands.size - 1) {
+      console.log('Invalid input');
+      showCurrentDirectory();
+    }
+    return true;
+  });
+}
+
+export function checkInput(username) {
+  stdin.on('data', data => {
+    if (/\.exit[\n\r]/.test(data.toString().toLowerCase())) {
+      process.exit();
+    }
+    checkInputData(data.slice(0, -1));
+  });
+  process.on('exit', () => console.log(`Thank you for using File Manager, ${ username }, goodbye!`));
+  process.on('SIGINT', () => {
+    console.log(' ');
+    process.exit();
+  });
+}
+
+export function showCurrentDirectory() {
+  console.log(`You are currently in ${ process.cwd() }`);
+}
+
+export const sort = (a, b) => {
+  const first = a.toUpperCase();
+  const second = b.toUpperCase();
+  if (first < second) return -1;
+  if (first > second) return 1;
+  return 0;
+};
