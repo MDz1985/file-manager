0d704aece98c2505cd7ec116756bacc48d87f1af
MDz1985 <m_d@mail.uk>
2024-02-03 17:35:36 +0100

    feat: implement navigation functionality

---
+++ modules/navigation.js
@@ -0,0 +1,45 @@
+import { chdir, cwd } from "node:process";
+import { showCurrentDirectory, sort } from "../services/file-manager-service.js";
+import { readdir, stat } from "node:fs";
+import { resolve } from "node:path";
+
+function up() {
+  chdir('../');
+  showCurrentDirectory();
+}
+
+function cd(path) {
+  if (!path || path.includes(' ')) {
+    console.log('Wrong input')
+  } else {
+    try {
+      chdir(path);
+    } catch {
+      console.log('Wrong input')
+    }
+    showCurrentDirectory();
+  }
+}
+
+function ls() {
+  readdir(cwd(), (err, filesArray) => {
+    const directories = [];
+    const files = [];
+    filesArray.forEach((file, i) => {
+      stat(resolve(cwd(), file), (err, stats) => {
+        stats?.isFile() ? files.push(file) : directories.push(file)
+        if (i === filesArray.length - 1) {
+          files.sort(sort);
+          directories.sort(sort);
+          const table = directories.map((dir) => ({ name: dir, type: 'directory' })).concat(
+            files.map((file) => ({ name: file, type: 'file' }))
+          )
+          console.table(table);
+          showCurrentDirectory();
+        }
+      })
+    })
+  })
+}
+
+export const nav = { up, cd, ls }
